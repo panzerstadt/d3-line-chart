6d89f99887dec42f85f3b818a29ecc53dd790454
panzerstadt <tliqun@gmail.com>
2018-06-12 12:30:05 +0900

    mulitple lines

--- src/App.js
+++ src/App.js
@@ -6,25 +6,109 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import LineChart from "./js/linechart.js";
+import LineChart from "./js/linechart";
+
+import { timeFormat } from 'd3-time-format';
+
+import json_data from "./data/dummy2";
+const affinities_data = json_data.results.hourly.content.affinities;
+const formatDate = timeFormat("%b %d, '%y");
+
+const r = affinities_data.chart_recent;
+const h = affinities_data.chart_historical;
+
+// output required
+/*
+
+[
+  [{label:thing, date:date, score:score}, ...]
+  [{label:thing, date:date, score:score}, ...]
+  ...
+]
 
 
-//render()
+*/
 
+// let pos = [];
+
+// data.forEach((v,i)=> {
+
+//   v.result.forEach((w,j)=> {
+
+//     w.label = v.label;
+//     pos.push(w);
+
+//   })
+
+// })
+
+// // label == date
+// const list_affinities_recent = r[0].slice(1, r[0].length);  // affinities labels
+// const list_affinities_historical = h[0].slice(1, h[0].length);
+
+// let data_formatted = []
+// const list_of_lines = []
+// r.forEach(element => {
+//   let affinities = r.slice(1, r.length); // cuts away the date label, keeps data
+
+//   let list_label_data = affinities.map(((v,i) => {
+//     list_of_lines.push()
+//   }));
+
+//   // each row
+//   recent_affinities.forEach(element => {
+//     data_1.push({
+//       label: formatDate(element[0]),
+//       score: element[1]
+//     })
+//   });
+  
+// });
+
+const list_recent_affinities_label = r[0].slice(1, r[0].length)  // all affinities labels
+const table_data = r.slice(1, r.length);
+const r_affinities_all = list_recent_affinities_label.map((v, i) => {
+  // make the dataset by each column
+  let data_i = table_data.map((w,j) => {
+    let date = w[0];
+    let score = w[i+1];  // this is wrong
+    let label = v;
+    return {
+      label: label,
+      date: date,
+      score: score
+    }
+  });
+  return data_i; // append to the full dataset
+});
+
+
+console.log(r_affinities_all[0]);
+
+
+
+//console.log(recent_affinities);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LineChart/>
+        <div>
+          <LineChart
+            label={'hey dog'}
+            data={r_affinities_all}
+            width={800}
+            height={480}
+            margin={{
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0
+            }}
+            />  
+        </div>    
       </div>
+
     );
   }
 }
--- src/js/linechart.js
+++ src/js/linechart.js
@@ -1,66 +1,92 @@
 
+// dummy data
+import { appleStock } from "@vx/mock-data";
+
 import React, { Component } from 'react';
 import { LinePath, Line, Bar } from "@vx/shape";
-import { Marker } from "@vx/marker";
-import { appleStock } from "@vx/mock-data";
-import { scaleTime, scaleLinear } from "@vx/scale";
+import { scaleTime, scaleLinear, scaleLog, scalePower } from "@vx/scale";
+import { GridRows, GridColumns } from '@vx/grid';
 import { localPoint } from "@vx/event";
 import { extent, max, bisector } from "d3-array";
+import { withTooltip, Tooltip } from '@vx/tooltip';
+import { timeFormat } from 'd3-time-format';
+
 
 // get data
-const stock = appleStock.slice(800);
+//const stock = appleStock.slice(800);
+const formatDate = timeFormat("%b %d, '%y");
 
-const xSelector = d => new Date(d.date);
-const ySelector = d => d.close;
-const bisectDate = bisector(xSelector).left;
+// accessors
+const xData = d => new Date(d.date);
+const yData = d => d.score;
+const bisectDate = bisector(d => new Date(d.date)).left;
 
 
-class lineChartVX extends Component {
-    state = {
-        position: null,
-    };
+class Area extends Component {
+    constructor(props) {
+        super(props);
+        this.handleTooltip = this.handleTooltip.bind(this);
+    }
 
-    handleDrag = ({ event, data, xSelector, xScale, yScale }) => {
-        const { x } = localPoint(event);  // screen point
+    handleTooltip({ event, data, xData, xScale, yScale }) {
+        const { showTooltip } = this.props;
+        const { x } = localPoint(event);
         const x0 = xScale.invert(x);
-        let index = bisectDate(data, x0, 1);  //?
-        const d0 = data[index -1];
+        const index = bisectDate(data, x0, 1);
+        const d0 = data[index - 1];
         const d1 = data[index];
         let d = d0;
         if (d1 && d1.date) {
-            if (x0 - xSelector(d0) > xSelector(d1) - x0) {
-                d = d1;
-            } else {
-                d = d0;
-                index = index -1;
-            }
+          d = x0 - xData(d0.date) > xData(d1.date) - x0 ? d1 : d0;
         }
-    
-        this.setState({
-            position: {
-                index, 
-                x: xScale(xSelector(d)),
-            },
+        showTooltip({
+          tooltipData: d,
+          tooltipLeft: x,
+          tooltipTop: yScale(d.score),
         });
     };
 
     render(){
-        const { position } = this.state;
+        const {
+            width,
+            height,
+            margin,
+
+            showTooltip,
+            hideTooltip,
+            tooltipData,
+            tooltipTop,
+            tooltipLeft,
+            events,
+        } = this.props;
+        if (width < 10) return null;
+
+        const {
+            data,
+        } = this.props;
+        //console.log(data);
+
+        // bounds
+        const xMax = width - margin.left - margin.right;
+        const yMax = height - margin.top - margin.bottom;
+
+
+        //const { position } = this.state;
         // arbitrary
-        const width = 500;
-        const height = 300;
+        // const width = 500;
+        // const height = 300;
 
         // scale to fit the data into a view height
         // (500 from above)
         // domain == our data
         // extent() from d3 takes 2 lists and returns
         // min for first, max for second
-        const xScale = scaleTime({
-        range: [0, width],
-        domain: extent(stock, xSelector),
-        });
+        // const xScale = scaleTime({
+        // range: [0, width],
+        // domain: extent(data, xData),
+        // });
 
-        const yMax = max(stock, ySelector);
+        //const yMax = max(stock, yStock);
 
         // swapped range() so that 0 is at the bottom
         //  and max (300) is at the top
@@ -68,28 +94,59 @@ class lineChartVX extends Component {
         // we need to add padding to the top
         // otherwise the max value is gonna hit the top
         // of the screen
-        const yScale = scaleLinear({
-        range: [height, 0],
-        domain: [0, yMax + (yMax + 4)],
-        });
+        // const yScale = scalePower({
+        // range: [height, 0],
+        // domain: [0, max(data, yData) + yMax / 3],
+        // nice: true,
+        // });
 
         //console.log(position ? stock[position.index] : 'none');
-        let markerData = position ? stock[position.index] : 'none';
-        let markerLabel = position ? `${markerData.date.slice(0, 10)} value:${markerData.close}` : 'none';
+        //let markerData = position ? stock[position.index] : 'none';
+        //let markerLabel = position ? `${markerData.date.slice(0, 10)} value:${markerData.close}` : 'none';
+
+        // make a jsx list of hashtags
+        let xScale;
+        let yScale;
+        const listLinePaths = data.map((v,i) => {
+            xScale = scaleTime({
+                range: [0, width],
+                domain: extent(v, xData),
+            });
+            yScale = scaleLinear({
+                range: [height, 0],
+                domain: [0, max(v, yData) + yMax / 3],
+                nice: true,
+            });
+
+            //const {label, date, score} = v;
+            return (<LinePath
+                key={i}
+                data={v}
+                xScale={xScale}
+                yScale={yScale}
+                x={xData}  // i'm guessing these are labels
+                y={yData}
+                strokeWidth={1}
+                stroke="#2AB0EA"
+            />)
+      
+          })
+
+        //   <LinePath
+        //             data={data}
+        //             xScale={xScale}
+        //             yScale={yScale}
+        //             x={xData}  // i'm guessing these are labels
+        //             y={yData}
+        //             strokeWidth={2}
+        //             stroke="#2AB0EA"
+        //         />
         
         // JSX here
         return (
+            <div>
             <svg width={width} height={height}>
                 <rect x={0} y={0} width={width} height={height} fill="transparent" rx={14} />
-                <LinePath
-                    data={position ? stock.slice(0, position.index) : stock}
-                    xScale={xScale}
-                    yScale={yScale}
-                    x={xSelector}
-                    y={ySelector}
-                    strokeWidth={2}
-                    stroke="#2AB0EA"
-                />
 
                 {/* 
                 weird boolean operator JS thing
@@ -99,37 +156,35 @@ class lineChartVX extends Component {
                 - if position can be coerced into false, return position
                 - otherwise return <Line..etc/>
                 */}
-                {position && (
-                    <LinePath
-                        data={stock.slice(position.index)}
-                        xScale={xScale}
-                        yScale={yScale}
-                        x={xSelector}
-                        y={ySelector}
-                        strokeWidth={1}
-                        stroke="rgba(0,0,0,0.1)"
-                    />
-                )}
-
+                <GridRows
+                    lineStyle={{ pointerEvents: 'none' }}
+                    scale={yScale}
+                    width={xMax}
+                    strokeDasharray="2,2"
+                    stroke="rgba(0,0,0,0.3)"
+                />
+                <GridColumns
+                    lineStyle={{ pointerEvents: 'none' }}
+                    scale={xScale}
+                    height={yMax}
+                    strokeDasharray="2,2"
+                    stroke="rgba(0,0,0,0.3)"
+                />
                 
+                {/* input line
+                find out how to JSX an array
+                */}
+                {/* <LinePath
+                    data={data}
+                    xScale={xScale}
+                    yScale={yScale}
+                    x={xData}  // i'm guessing these are labels
+                    y={yData}
+                    strokeWidth={2}
+                    stroke="#2AB0EA"
+                /> */}
 
-                {position && (
-                    // <Line
-                    //     from={{ x: position.x, y: 0 }}
-                    //     to={{ x: position.x, y: height }}
-                    //     strokeWidth={2}
-                    //     stroke="#E2615F"
-                    // />
-                    <Marker
-                        from={{ x: position.x, y: 0 }}
-                        to={{ x: position.x, y: height }}
-                        stroke={"#E2615F"}
-                        label={markerLabel}
-                        labelStroke={'none'}
-                        labelDx={6}
-                        labelDy={15}
-                    />
-                )}
+                {listLinePaths}
 
                 
 
@@ -140,44 +195,97 @@ class lineChartVX extends Component {
                     height={height}
                     fill="transparent"
                     rx={14}
-                    data={stock}
+                    data={data[1]}
                     onTouchStart={data => event =>
-                        this.handleDrag({
-                            event,
-                            data,
-                            xSelector,
-                            xScale,
-                            yScale,
-                        })}
+                    this.handleTooltip({
+                        event,
+                        data,
+                        xData,
+                        xScale,
+                        yScale,
+                    })}
                     onTouchMove={data => event =>
-                        this.handleDrag({
-                            event,
-                            data,
-                            xSelector,
-                            xScale,
-                            yScale,
-                        })}
+                    this.handleTooltip({
+                        event,
+                        data,
+                        xData,
+                        xScale,
+                        yScale,
+                    })}
                     onMouseMove={data => event =>
-                        this.handleDrag({
-                            event,
-                            data,
-                            xSelector,
-                            xScale,
-                            yScale,
-                        })}
-                    onTouchEnd={data => event =>
-                        this.setState({
-                            position: null
-                        })}
-                    onMouseLeave={data => event =>
-                        this.setState({
-                            position: null
-                        })}
+                    this.handleTooltip({
+                        event,
+                        data,
+                        xData,
+                        xScale,
+                        yScale,
+                    })}
+                    onMouseLeave={data => event => hideTooltip()}
                 />
+                
+                {/* the pretty dotted line with white and blue dot thing */}
+                {tooltipData && (
+                    <g>
+                    <Line
+                        from={{ x: tooltipLeft, y: 0 }}
+                        to={{ x: tooltipLeft, y: yMax }}
+                        stroke="rgba(92, 119, 235, 1.000)"
+                        strokeWidth={2}
+                        style={{ pointerEvents: 'none' }}
+                        strokeDasharray="2,2"
+                    />
+                    <circle
+                        cx={tooltipLeft}
+                        cy={tooltipTop + 1}
+                        r={4}
+                        fill="black"
+                        fillOpacity={0.1}
+                        stroke="black"
+                        strokeOpacity={0.1}
+                        strokeWidth={2}
+                        style={{ pointerEvents: 'none' }}
+                    />
+                    <circle
+                        cx={tooltipLeft}
+                        cy={tooltipTop}
+                        r={4}
+                        fill="rgba(92, 119, 235, 1.000)"
+                        stroke="white"
+                        strokeWidth={2}
+                        style={{ pointerEvents: 'none' }}
+                    />
+                    </g>
+                )}
 
             </svg>
+
+            {/* the white box date label thing */}
+            {tooltipData && (
+                <div>
+                <Tooltip
+                    top={tooltipTop - 12}
+                    left={tooltipLeft + 12}
+                    style={{
+                    backgroundColor: 'rgba(92, 119, 235, 1.000)',
+                    color: 'white',
+                    }}
+                >
+                    {`score: ${yData(tooltipData)}`}
+                </Tooltip>
+                <Tooltip
+                    top={yMax - 14}
+                    left={tooltipLeft}
+                    style={{
+                    transform: 'translateX(-50%)',
+                    }}
+                >
+                    {formatDate(xData(tooltipData))}
+                </Tooltip>
+                </div>
+            )}
+            </div>
         );
     }
 }
 
-export default lineChartVX;
+export default withTooltip(Area);
